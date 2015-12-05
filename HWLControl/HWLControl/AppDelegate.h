7279fbd7ababa146c16032fbc31d03600a719e01
git <eorient@sina.com>
2015-12-05 19:43:38 +0800

    初步创建工程
    首先创建一个工程，用来创建控件代码

---
+++ HWLControl/HWLControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWLControl
+//
+//  Created by 侯卫磊 on 15/12/5.
+//  Copyright © 2015年 侯卫磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWLControl/HWLControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWLControl
+//
+//  Created by 侯卫磊 on 15/12/5.
+//  Copyright © 2015年 侯卫磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
