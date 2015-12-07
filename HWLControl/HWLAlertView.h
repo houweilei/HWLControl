39d2395b1685d90e4f382c4f3972be020d642d54
git <eorient@sina.com>
2015-12-07 21:17:17 +0800

    增加HWLAlertView的文件

---
+++ HWLControl/HWLAlertView.h
@@ -0,0 +1,13 @@
+//
+//  HWLAlertView.h
+//  HWLControl
+//
+//  Created by 侯卫磊 on 15/12/7.
+//  Copyright © 2015年 侯卫磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWLAlertView : UIView
+
+@end
---
+++ HWLControl/HWLAlertView.m
@@ -0,0 +1,21 @@
+//
+//  HWLAlertView.m
+//  HWLControl
+//
+//  Created by 侯卫磊 on 15/12/7.
+//  Copyright © 2015年 侯卫磊. All rights reserved.
+//
+
+#import "HWLAlertView.h"
+
+@implementation HWLAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
